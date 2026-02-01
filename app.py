cc2b86936d6a8955f5b2b702b0661784de7c3cb7
Nijanshu <nijanshusingh@gmail.com>
2026-02-02 03:43:30 +0530

    Flask Todo App

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    "sqlite:///" + os.path.join(basedir, "todo.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        # print(request.form['title'])
+        todo=Todo(title=title,desc=desc)
+
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")  # Prevent form resubmission
+
+    allTodo=Todo.query.all()
+    print("allTodo")
+    return render_template('index.html',allTodo=allTodo)
+
+@app.route("/products")
+def prods():
+    l = 10
+    return f"<p>Welcome to our products</p> {l,9}"
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        # print(request.form['title'])
+
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html',todo=todo)
+    
+    
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+# ✅ CREATE DATABASE TABLES
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    # exit()
+
+import os
+print("cwd",os.getcwd())
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
